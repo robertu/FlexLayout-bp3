7cf29191f60c826ebe900955371cef5cb6815e42
Robert Urbańczyk <robertu@robertu.net>
2019-10-24 19:54:44 +0200

    cosmetics

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,51 @@
 
 module.exports = {
-	mode:'development',
-	entry: {
-		demo: "./examples/demo/App.tsx",
-	    tests: "./spec/main.ts"
-	},
+  mode:'development',
+  entry: {
+    demo: "./examples/demo/App.tsx",
+      tests: "./spec/main.ts"
+  },
 
-	output: {
-		path: __dirname,
-		filename: "./bundles/[name].js"
-	},
+  output: {
+    path: __dirname,
+    filename: "./bundles/[name].js"
+  },
 
-	resolve: {
+  resolve: {
         // Add '.ts' and '.tsx' as resolvable extensions.
         extensions: [".ts", ".tsx", ".js", ".json"]
     },
 
-	watch:true,
+  watch:true,
 
-	devServer: {
-		contentBase: '.'
-	   },
+  devServer: {
+    contentBase: '.'
+     },
 
-	devtool: 'source-map',
+  devtool: 'source-map',
 
     module: {
         rules: [
             // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
             { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
-						{
-							test: /\.less$/,
-							use: [
-								{
-									loader: 'style-loader',
-								},
-								{
-									loader: 'css-loader',
-								},
-								{
-									loader: 'less-loader',
-									options: {
-										strictMath: false,
-										noIeCompat: true,
-									},
-								},
-							],
-						},
+            {
+              test: /\.less$/,
+              use: [
+                {
+                  loader: 'style-loader',
+                },
+                {
+                  loader: 'css-loader',
+                },
+                {
+                  loader: 'less-loader',
+                  options: {
+                    strictMath: false,
+                    noIeCompat: true,
+                  },
+                },
+              ],
+            },
             // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
             { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
         ]
--- webpack_build.config.js
+++ webpack_build.config.js
@@ -1,29 +1,29 @@
 module.exports = {
-	entry: {
-		flexlayout: "./src/index.ts"
-	},
+  entry: {
+    flexlayout: "./src/index.ts"
+  },
 
-	output: {
-		path: __dirname,
-		library: "FlexLayout",
-		libraryTarget:"umd",
-		filename: "./dist/[name].js"
-	},
-	externals: {
-		react: {
-			root: 'React',
-			commonjs: 'react',
-			commonjs2: 'react',
-			amd: 'react'
-		},
-		"react-dom":{
-			root: 'ReactDOM',
-			commonjs: 'react-dom',
-			commonjs2: 'react-dom',
-			amd: 'react-dom'
-		}
-	},
-	resolve: {
+  output: {
+    path: __dirname,
+    library: "FlexLayout",
+    libraryTarget:"umd",
+    filename: "./dist/[name].js"
+  },
+  externals: {
+    react: {
+      root: 'React',
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react'
+    },
+    "react-dom":{
+      root: 'ReactDOM',
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'react-dom'
+    }
+  },
+  resolve: {
         // Add '.ts' and '.tsx' as resolvable extensions.
         extensions: [".ts", ".tsx", ".js", ".json"]
     },
@@ -31,24 +31,24 @@ module.exports = {
         rules: [
             // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
             { test: /\.tsx?$/, loader: "awesome-typescript-loader"},
-						{
-							test: /\.less$/,
-							use: [
-								{
-									loader: 'style-loader',
-								},
-								{
-									loader: 'css-loader',
-								},
-								{
-									loader: 'less-loader',
-									options: {
-										strictMath: false,
-										noIeCompat: true,
-									},
-								},
-							],
-						},
+            {
+              test: /\.less$/,
+              use: [
+                {
+                  loader: 'style-loader',
+                },
+                {
+                  loader: 'css-loader',
+                },
+                {
+                  loader: 'less-loader',
+                  options: {
+                    strictMath: false,
+                    noIeCompat: true,
+                  },
+                },
+              ],
+            },
             // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
             { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
         ]
